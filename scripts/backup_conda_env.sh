c2f7e4851c4e64259643abe6b77ceaee40780701
mysafesky <mysafesky@gmail.com>
2019-07-22 22:50:49 -0700

    revise backup script, create new tf backup

--- scripts/backup_conda_env.sh
+++ scripts/backup_conda_env.sh
@@ -1,5 +1,5 @@
 today=`date +%Y%m%d%H%M%S`
-filename=$CONDA_DEFAULT_ENV.yml.$today
+filename=$CONDA_DEFAULT_ENV.$today.yml
 conda env export | grep -v "^prefix: "> $filename
 mv $filename ../env_yaml/
 unset today
